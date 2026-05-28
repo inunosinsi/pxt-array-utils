7baf2d59eef22606d50dd213c3f8a720de4ff62f
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2026-05-28 14:56:47 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
-})
+namespace pxtArrayUtils {
+
+    //% block="%array から最大値を調べる"
+    export function max (array: number[]): number {
+        if (array.length == 0) {
+            return 0
+        }
+        let tmp = array[0]
+        for (let v of array) {
+            if (v > tmp) {
+                tmp = v
+            }
+        }
+        return tmp
+    }
+}
